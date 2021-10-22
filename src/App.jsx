2b1f609c69a31441e035e2316829bfc56b823c55
Tokov <flyingcatsad@gmail.com>
2021-10-22 09:45:08 +0300

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import Header from "./components/Header";
+import UsersPage from "./components/UsersPage";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      <UsersPage />
+    </div>
+  );
+}
+
+export default App;
